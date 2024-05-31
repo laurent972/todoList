23c46c602f5a1993c8d92f016479e00d1c455179
lrega81 <l.rega@dispofi.fr>
2024-05-31 17:44:36 +0200

    mongo

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,10 @@
 const express = require ("express");
 const port = 5500;
 
+const mongoose = require('mongoose')
+
 const app = express();
+mongoose.connect('mongodb+srv://fs:>8,Ry)+GGgMgv!i@todolist.jf5tkn2.mongodb.net/?retryWrites=true&w=majority&appName=todolist');
 
 //middleware
 app.use(express.json())
@@ -10,3 +13,5 @@ app.use(express.urlencoded({extended:false}))
 app.use("/tasks", require("./routes/tasks.routes"));
 
 app.listen(port, () => console.log('le serveur a démarré sur ' + port))
+
+
