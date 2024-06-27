231f95df5bfa46c31274d50844ca026fb5a41828
lrega81 <l.rega@dispofi.fr>
2024-06-27 22:57:27 +0200

    dfdf

--- src/app/api/load.todos.js
+++ src/app/api/load.todos.js
@@ -1,5 +1,5 @@
 export default async function listesTodos() {
-    const reponse = await fetch("https://todo-list-72pe.vercel.app/tasks");
+    const reponse = await fetch("https://todo-list-72pe.vercel.app/tasks/");
     const tasks = await reponse.json();
     return tasks
   }
