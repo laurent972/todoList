acd57053dd866341f72e18b49b9acd3df747a458
lrega81 <l.rega@dispofi.fr>
2024-07-01 15:49:39 +0200

    correction

--- src/app/(public)/register/page.jsx
+++ src/app/(public)/register/page.jsx
@@ -44,7 +44,7 @@ const Register = () =>{
                                 d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z"
                                 clipRule="evenodd" />
                             </svg>
-                            <input type="password" className="grow" value="password" />
+                            <input type="password" className="grow" value="verif-password" />
                         </label>
                             <input type="submit" className={'bg-blue-500 hover:bg-blue-800 p-2 mt-5 rounded-lg text-white cursor-pointer'}/>
                     </form>
