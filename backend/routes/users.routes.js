da9643302c82216faa5c1082231f5b48b8cbbedb
lrega81 <l.rega@dispofi.fr>
2024-07-22 13:43:30 +0200

    userRoutes

--- backend/routes/users.routes.js
+++ backend/routes/users.routes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const authController = require('../controllers/auth.controller');
 
-router.post("/register", authController.signUp);
+router.post("/users/register", authController.signUp);
 
 module.exports = router;
