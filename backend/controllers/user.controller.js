18fbae6847dfd311811e8afcf8c5a9d6c4822e48
lrega81 <l.rega@dispofi.fr>
2024-07-26 18:08:12 +0200

    user

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -5,3 +5,23 @@ module.exports.getAllUsers = async(req,res) =>{
     const users = await UserModel.find().select('-password');
     res.status(200).json(users);
 }
+
+module.exports.userInfo =  async (req, res) => {
+    console.log(req.params);
+    if(!ObjectId.isValid(req.params.id))
+        return res.status(400).send('Id unknown : '+ req.params.id)
+
+   
+        try {
+          const doc = await UserModel.findById(req.params.id);
+          if (doc) {
+            console.log('Document found:', doc);
+            return res.status(400).send(doc)
+          } else {
+            console.log('Document not found');
+          }
+        } catch (err) {
+          console.error('Error:', err);
+        }
+    
+}
--- backend/routes/users.routes.js
+++ backend/routes/users.routes.js
@@ -5,5 +5,6 @@ const userController = require ('../controllers/user.controller');
 router.post("/register", authController.signUp);
 
 router.get('/', userController.getAllUsers);
+router.get('/:id', userController.userInfo);
 
 module.exports = router;
