d0a0962082dfcd033a06a080672346cbfeba2d85
lrega81 <l.rega@dispofi.fr>
2024-08-23 14:58:14 +0200

    fix error

--- src/app/_components/Login.js
+++ src/app/_components/Login.js
@@ -71,7 +71,7 @@ const Login = () =>{
             </form>
             <div className="pt-8">
                 <p className="text-md font-bold">Pas encore de compte ?</p>
-                <Link href={"/register"} className="underline text-blue-500"> S'enregistrer</Link>
+                <Link href={"/register"} className="underline text-blue-500"> S&apos;enregistrer</Link>
             </div>
             
         </div>            
