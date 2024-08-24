256e5240d402fd1daf87cdd371a243d5d2c3790e
lrega81 <l.rega@dispofi.fr>
2024-08-24 20:11:02 +0200

    requete

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ const app = express();
 
 
 const corsOptions = {
-    origin: 'http://localhost:3000', // Change to your frontend's URL
+    origin: /*'http://localhost:3000',*/'https://todo-list-9x4c-fbve8wmil-laurents-projects-5cda7e0d.vercel.app/', // Change to your frontend's URL
     credentials: true, // Allow credentials (cookies, authorization headers, etc.)
   };
 
