833d85031c6026cff2a07a8559c38278b38631e9
lrega81 <122445558+lrega81@users.noreply.github.com>
2024-09-06 16:49:30 +0200

    db

--- backend/config/db.js
+++ backend/config/db.js
@@ -7,7 +7,10 @@ connectDB().catch(err => {
 
 
 async function connectDB() {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
     console.log(' connected')
   }
 
