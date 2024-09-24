648a212b6a3a2138c9e9c93a0f17ef491eda57be
lrega81 <122445558+lrega81@users.noreply.github.com>
2024-09-24 12:04:19 +0200

    cors

--- backend/index.js
+++ backend/index.js
@@ -24,7 +24,7 @@ const app = express();
 
 
 connectDB();
-app.use(cors(corsOptions))
+app.use('*', cors(corsOptions))
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(cookieParser());
