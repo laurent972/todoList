b960db9e8f9d103b2b2868d61458c1d920441db3
lrega81 <122445558+lrega81@users.noreply.github.com>
2024-09-24 11:26:44 +0200

    load

--- src/app/api/load.todos.js
+++ src/app/api/load.todos.js
@@ -1,5 +1,5 @@
 export default async function listesTodos() {
-    const reponse = await fetch("http://localhost:5500/tasks");
+    const reponse = await fetch(process.env.NEXT_PUBLIC_SERVER_URL || "http://localhost:5500/tasks");
     const tasks = await reponse.json();
     return tasks.sort((a, b) => new Date(b.createDate) - new Date(a.createDate))
   }
