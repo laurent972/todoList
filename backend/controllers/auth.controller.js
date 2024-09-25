bb199f9877674a08696bd86bb908c0bd45c3ade8
lrega81 <122445558+lrega81@users.noreply.github.com>
2024-09-25 15:45:24 +0200

    requete

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -28,6 +28,7 @@ module.exports.signUp = async (req, res) => {
 
 module.exports.signIn = async (req, res) => {
     const { email, password } = req.body;
+    res.setHeader('Access-Control-Allow-Origin', 'https://todo-list-two-beta-78.vercel.app/')
 
     try {
         // Tenter de connecter l'utilisateur
