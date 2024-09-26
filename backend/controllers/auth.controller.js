402c8878974dede1b53b583501de95624d59b4c8
lrega81 <122445558+lrega81@users.noreply.github.com>
2024-09-26 11:04:53 +0200

    token validation

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -36,8 +36,17 @@ module.exports.signIn = async (req, res) => {
         // Générer un token JWT pour l'utilisateur
         const token = createToken(user._id); // Assurez-vous que `createToken` fonctionne correctement
 
-        // Définir le cookie JWT
-        res.cookie('jwt', token, {httpOnly: true, maxAge });
+        // Définir le cookie JWT for local
+        //res.cookie('jwt', token, {httpOnly: true, maxAge });
+
+        res.cookie('jwt', token, {
+            httpOnly: true,        // Le cookie ne sera pas accessible via JavaScript (plus sécurisé)
+            secure: true,          // Le cookie sera uniquement envoyé via HTTPS ** disable en local
+            sameSite: 'None',      // Autorise les requêtes cross-site
+            maxAge: 24 * 60 * 60 * 1000 // Durée de vie du cookie (1 jour ici)
+          });
+        
+          res.status(200).json({ message: 'Authentification réussie' });
 
         // Répondre avec succès
         res.status(200).json({ message: 'Login successful', user: user._id });
