30edb9304b665c0f5b9bc9628eb57e2c1f679b62
laurent972 <laurent.digitick@gmail.com>
2025-01-23 15:23:22 +0100

    test

--- src/app/_components/Header.js
+++ src/app/_components/Header.js
@@ -1,7 +1,38 @@
-
+'use client'
+import cookie from "js-cookie"
+import { useRouter } from 'next/navigation';
 
 const Header = () => {
-  
+    const router = useRouter(); 
+
+    const removeCookie = (key) =>{
+        if(window != 'undefined'){
+            cookie.remove(key,{expires:1});
+        }
+    }
+
+    const logout = async (e) =>{
+        e.preventDefault();
+          try{  
+            const response = await fetch('http://localhost:5500/users/logout', {
+                method: 'get',
+                headers: {
+                  "Content-Type": "application/json",
+                  "Access-Control-Allow-Origin": "*",
+                },
+                credentials:"include",
+            });
+            console.log(response);    
+            if(response.ok){
+                removeCookie('jwt')
+                router.push('/')
+            }
+          }catch(err){
+            console.log(err);
+          }  
+    }
+
+
     return(
         <header>
             <div className="navbar bg-base-100">
@@ -9,7 +40,9 @@ const Header = () => {
                     <a className="btn btn-ghost text-xl">TodoList</a>
                 </div>
                 <div className="flex-none gap-2">
-                 
+                    <form onSubmit={logout}>
+                        <button type="submit">Logout</button> 
+                    </form>
                 </div>
                 
             </div>
